2c326dfe597e6b8daaafbbc09472e57f328f52c7
Ernest45 <gkgkswns71@gmail.com>
2024-01-06 21:21:05 +0900

    1/6 Provider or Proxy로 requestScope처리

--- core/src/main/java/hello/core/web9_2/controller/LogDemoController.java
+++ core/src/main/java/hello/core/web9_2/controller/LogDemoController.java
@@ -14,15 +14,23 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
+//    private final MyLogger myLogger;
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody // view 화면이 없어서 문자로 바로 반환을 위해 ( 보통은 요청이오면 view 템플릿 거쳐 랜더링돼서 나가야하지만..)
-    public String logDemo(HttpServletRequest request) { // 자바에서 제공하는 서블릿 규약인 request 정보를 받을 수 있다
+    public String logDemo(HttpServletRequest request) throws InterruptedException { // 자바에서 제공하는 서블릿 규약인 request 정보를 받을 수 있다
         String requestURL = request.getRequestURI().toString(); //고객이 어떤 URL로 요청한 지 알 수 있다.
+
+        System.out.println("myLogger = " + myLogger.getClass());
+
+//        MyLogger myLogger = myLoggers.getObject();
         myLogger.setRequestURL(requestURL); //로그 남을 때 uri까지 같이 반환하기 위해
 
         myLogger.log("controller test");
+
+        Thread.sleep(1000);
+
         logDemoService.logic("testId");
         return "Ok";
 
--- core/src/main/java/hello/core/web9_2/service/LogDemoService.java
+++ core/src/main/java/hello/core/web9_2/service/LogDemoService.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+        private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggers;
+
     public void logic(String id) {
+
+//        MyLogger myLogger = myLoggers.getObject();
         myLogger.log("service id = " + id);
 
 
